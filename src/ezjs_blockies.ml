a9840831e9b814ea8228bbe54a97ba2b1d15e8db
Maxime Levillain <maxime.levillain@origin-labs.com>
2020-10-23 10:38:30 +0200

    call blockies only on calls

--- src/ezjs_blockies.ml
+++ src/ezjs_blockies.ml
@@ -19,12 +19,6 @@ let optdef f = function
   | None -> undefined
   | Some x -> def (f x)
 
-let convdef f x = match Optdef.to_option x with
-  | None -> undefined
-  | Some x -> def (f x)
-
-let blockies : blockies t = Unsafe.variable "blockies"
-
 let make_opts ?seed ?color ?bgcolor ?size ?scale ?spotcolor () : blockies_options t =
   object%js
     val seed = optdef string seed
@@ -35,10 +29,22 @@ let make_opts ?seed ?color ?bgcolor ?size ?scale ?spotcolor () : blockies_option
     val spotcolor = optdef string spotcolor
   end
 
+let blockies () : blockies t = Unsafe.variable "blockies"
+let blockies_opt () : blockies t option = Optdef.to_option @@ Unsafe.variable "blockies"
+
+let ready ?(not_found=fun () -> ()) ?(timeout=500.) f =
+  match blockies_opt () with
+  | None ->
+    let cb () = match blockies_opt () with
+      | None -> not_found ()
+      | Some b -> f b in
+    ignore @@ Dom_html.window##setTimeout (wrap_callback cb) timeout
+  | Some b -> f b
+
 let create ?seed ?color ?bgcolor ?size ?scale ?spotcolor () =
   let options = make_opts ?seed ?color ?bgcolor ?size ?scale ?spotcolor () in
-  blockies##create options
+  (blockies ())##create options
 
 let render ?seed ?color ?bgcolor ?size ?scale ?spotcolor canvas =
   let options = make_opts ?seed ?color ?bgcolor ?size ?scale ?spotcolor () in
-  blockies##render options canvas
+  (blockies ())##render options canvas
